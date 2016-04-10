af4df54e95815069321f45910d52d9d2ed418187
Travis D <travis.dacosta1@gmail.com>
2016-04-10 08:18:19 -0400

    Static Method and Properties

---
+++ Kiwi.php
@@ -0,0 +1,10 @@
+<?php
+	class Kiwi {
+		public static $var = 'Fruits are tasty';	
+		public $var2=10;	
+		
+		public static function hello() {
+			return 'Hello World!!!!';		
+		}	
+	}
+?>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require 'Foo.php';
-
-require 'Bar.php';
-
-$foo = new Bar();
-
-echo $foo->baz();
+	require 'Kiwi.php';
+	
+	
+	//Doesn't require to be instantiated with the new keyword
+	echo Kiwi::hello();//Similar to the scope operator but only works for static methods and and variables
+	echo Kiwi::$var;
+	//echo Kiwi::$var2 //won't work because it is not a static property  	
 ?>
